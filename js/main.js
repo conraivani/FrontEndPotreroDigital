1de6db05b4fc260e441c084553cea25a83561ec1
conraivani <107953280+conraivani@users.noreply.github.com>
2022-11-23 13:45:07 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-// function modonocturno() {
-//     document.body.style.backgroundColor= "black";
-//     document.getElementById("body").style.backgroundColor = "black";
-//     document.getElementById("body").style.backgroundImage = "none";
-//     document.getElementById("body").style.color = "white";
-//     document.getElementById("main").style.backgroundColor = "black";
-//     document.getElementById("main").style.color = "white";
-//     document.getElementById("header").style.backgroundColor = "black";
-//     document.getElementById("header").style.color = "white";
-//     document.getElementById("nav").style.backgroundColor = "black";
-//     document.getElementById("nav").style.color = "white";
-//     document.getElementById("section").style.backgroundColor = "black";
-//     document.getElementById("section").style.color = "white";
-//     document.getElementById("aside").style.backgroundColor = "black";
-//     document.getElementById("aside").style.color = "white";
-//     document.getElementById("footer").style.backgroundColor = "black";
-//     document.getElementById("footer").style.color = "white";
+function modonocturno() {
+    document.body.style.backgroundColor= "black";
+    document.getElementById("body").style.backgroundColor = "black";
+    document.getElementById("body").style.backgroundImage = "none";
+    document.getElementById("body").style.color = "white";
+    document.getElementById("main").style.backgroundColor = "black";
+    document.getElementById("main").style.color = "white";
+    document.getElementById("header").style.backgroundColor = "black";
+    document.getElementById("header").style.color = "white";
+    document.getElementById("nav").style.backgroundColor = "black";
+    document.getElementById("nav").style.color = "white";
+    document.getElementById("section").style.backgroundColor = "black";
+    document.getElementById("section").style.color = "white";
+    document.getElementById("aside").style.backgroundColor = "black";
+    document.getElementById("aside").style.color = "white";
+    document.getElementById("footer").style.backgroundColor = "black";
+    document.getElementById("footer").style.color = "white";
     
     
-// }
-JavaScript
- 
-function cambiarModo() { 
-    var cuerpoweb = document.body; 
-    cuerpoweb.classList.toggle("oscuro"); 
 }
+
+ 
+// function cambiarModo() { 
+//     var cuerpoweb = document.body; 
+//     cuerpoweb.classList.toggle("oscuro"); 
+// }
 
 function derechos() {
     document.getElementById("creador").innerHTML = "Todos los derechos reservados ® Conrado Ivani 2022"
